aac894b84b89687e7d7dce3b8a6dcb88b60b0a9a
Tomasz Wysocki <0xdaydream@proton.me>
2023-12-27 05:11:34 +0100

    refactor: move main page.tsx to (overview)
    Moved page.tsx and part of layout.tsx (navbar) to render only on
    (overview) route group and not on (auth) route group.
    
    https://nextjs.org/docs/app/building-your-application/routing/route-groups#opting-specific-segments-into-a-layout

---
+++ app/(overview)/layout.tsx
@@ -0,0 +1,7 @@
+import Navbar from "@/components/navbar";
+
+export default function OverviewLayout() {
+    return (
+        <Navbar />
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Navbar></Navbar>
           {children}
         </ThemeProvider>
       </body>
