3f4aa658e4409daa17cf90306cc7af9baae019bc
Tomasz Wysocki <0xdaydream@proton.me>
2024-01-12 14:48:39 +0100

    feat: add transition effect using framer-motion

--- components/note-item.tsx
+++ components/note-item.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { Note } from '@prisma/client'
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card'
 import DeleteDialog from './delete-dialog'
@@ -6,6 +8,7 @@ import { Icons } from './icons'
 import { buttonVariants } from './ui/button'
 import { cn } from '@/lib/utils'
 import DateTime from './date-time'
+import { motion, AnimatePresence } from 'framer-motion'
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
     note: Pick<Note, 'id' | 'title' | 'content' | 'updatedAt'>
@@ -13,30 +16,34 @@ interface Props extends React.HTMLAttributes<HTMLDivElement> {
 
 const NoteItem = ({ note, ...props }: Props) => {
     return (
-        <Card {...props}>
-            <Link
-                href={`/edit/${note.id}`}
-                className={cn(
-                    buttonVariants({ variant: 'outline' }),
-                    'group-hover:inline-flex hidden absolute right-[3.75rem] top-3 h-10 w-10 p-0'
-                )}
-            >
-                <Icons.pencil className='h-5 w-5' />
-            </Link>
-            <DeleteDialog
-                note={note}
-                className='absolute right-3 top-3 h-10 w-10 p-0 group-hover:inline-flex hidden '
-            />
-            <CardHeader>
-                <CardTitle>{note.title}</CardTitle>
-                <DateTime note={note} />
-            </CardHeader>
-            <div>
-                <CardContent className='whitespace-break-spaces'>
-                    {note.content}
-                </CardContent>
-            </div>
-        </Card>
+        <AnimatePresence>
+            <motion.div layout>
+                <Card {...props}>
+                    <Link
+                        href={`/edit/${note.id}`}
+                        className={cn(
+                            buttonVariants({ variant: 'outline' }),
+                            'group-hover:opacity-100 duration-100 transition-opacity opacity-0 absolute right-[3.75rem] top-3 h-10 w-10 p-0'
+                        )}
+                    >
+                        <Icons.pencil className='h-5 w-5' />
+                    </Link>
+                    <DeleteDialog
+                        note={note}
+                        className='absolute right-3 top-3 h-10 w-10 p-0 group-hover:opacity-100 duration-100 transition-opacity opacity-0'
+                    />
+                    <CardHeader>
+                        <CardTitle>{note.title}</CardTitle>
+                        <DateTime note={note} />
+                    </CardHeader>
+                    <div>
+                        <CardContent className='whitespace-break-spaces'>
+                            {note.content}
+                        </CardContent>
+                    </div>
+                </Card>
+            </motion.div>
+        </AnimatePresence>
     )
 }
 
